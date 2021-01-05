d962406092ed4d0a5a4bff5b2a5541f293c4df59
kochetkov-ma <kochetkov-ma@yandex.ru>
2021-01-05 11:03:04 +0300

    fix: LoggedAllureLifecycle attachment display

--- src/main/kotlin/ru/iopump/kotest/LoggedAllureLifecycle.kt
+++ src/main/kotlin/ru/iopump/kotest/LoggedAllureLifecycle.kt
@@ -17,10 +17,11 @@ class LoggedAllureLifecycle : AllureLifecycle() {
     override fun addAttachment(name: String?, type: String?, fileExtension: String?, stream: InputStream?) {
         when (log.isDebugEnabled) {
             true -> stream.use { io ->
-                runCatching { io?.readAllBytes() ?: "".toByteArray(UTF_8) }.getOrElse { it.localizedMessage.toByteArray(UTF_8) }
+                runCatching { io?.readBytes() ?: "".toByteArray(UTF_8) }
+                    .getOrElse { it.localizedMessage.toByteArray(UTF_8) }
             }.apply {
                 super.addAttachment(name, type, fileExtension, this.inputStream()).also {
-                    log.debug("[ALLURE-ATTACHMENT] $name $type $fileExtension\n{}", toString(UTF_8).take(2000))
+                    log.debug("[ALLURE-ATTACHMENT] $name $type $fileExtension\n{}", this.toString(UTF_8).take(2000))
                 }
             }
             false -> super.addAttachment(name, type, fileExtension, stream)
@@ -31,15 +32,20 @@ class LoggedAllureLifecycle : AllureLifecycle() {
     private val StepResult?.log
         get() = when (this) {
             null -> ""
-            else -> "${name ?: ""} ${status ?: ""}" + params.entries
-                .takeIf { it.isNotEmpty() }
-                ?.joinToString("\n", "\n") { "${it.key}: ${it.value}" }
+            else -> "${name ?: ""} ${status ?: ""}" + (
+                    params.entries
+                        .takeIf { it.isNotEmpty() }
+                        ?.joinToString("\n", "\n") { "${it.key}: ${it.value}" } ?: ""
+                    )
         }
 
     private val StepResult?.params
         get() = mapOf(
             "description" to this?.description,
             "parameters" to this?.parameters?.joinToString(prefix = "[", postfix = "]") { "${it.name}=${it.value}" },
-            "attachments" to this?.attachments?.joinToString(prefix = "[", postfix = "]") { "${it.name}=${it.type}(${it.source.length})" }
+            "attachments" to this?.attachments?.joinToString(
+                prefix = "[",
+                postfix = "]"
+            ) { "${it.name}=${it.type}(${it.source.length})" }
         ).filterNot { it.value.isNullOrBlank() || it.value == "[]" }
 }
